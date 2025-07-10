45253698888a5d9fbf970d9bee89d9a39740c4aa
Mohamed <mhijazi@beamable.com>
2025-07-10 14:43:16 -0300

    - moved the sample to a new unreal project

---
+++ UnrealSample/Plugins/UnrealSuiSampleMicroserviceClients/Source/UnrealSuiSampleMicroserviceClients/Private/AutoGen/IncrementStatRequestArgsLibrary.cpp
@@ -0,0 +1,38 @@
+
+#include "UnrealSuiSampleMicroserviceClients/Public/AutoGen/IncrementStatRequestArgsLibrary.h"
+
+#include "CoreMinimal.h"
+
+
+FString UIncrementStatRequestArgsLibrary::IncrementStatRequestArgsToJsonString(const UIncrementStatRequestArgs* Serializable, const bool Pretty)
+{
+	FString Result = FString{};
+	if(Pretty)
+	{
+		TUnrealPrettyJsonSerializer JsonSerializer = TJsonStringWriter<TPrettyJsonPrintPolicy<TCHAR>>::Create(&Result);
+		Serializable->BeamSerialize(JsonSerializer);
+		JsonSerializer->Close();
+	}
+	else
+	{
+		TUnrealJsonSerializer JsonSerializer = TJsonStringWriter<TCondensedJsonPrintPolicy<TCHAR>>::Create(&Result);
+		Serializable->BeamSerialize(JsonSerializer);
+		JsonSerializer->Close();			
+	}
+	return Result;
+}	
+
+UIncrementStatRequestArgs* UIncrementStatRequestArgsLibrary::Make(int64 GamerTag, UObject* Outer)
+{
+	auto Serializable = NewObject<UIncrementStatRequestArgs>(Outer);
+	Serializable->GamerTag = GamerTag;
+	
+	return Serializable;
+}
+
+void UIncrementStatRequestArgsLibrary::Break(const UIncrementStatRequestArgs* Serializable, int64& GamerTag)
+{
+	GamerTag = Serializable->GamerTag;
+		
+}
+
---
+++ UnrealSample/Plugins/UnrealSuiSampleMicroserviceClients/Source/UnrealSuiSampleMicroserviceClients/Public/AutoGen/UpgradeItemRequestArgs.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Serialization/BeamJsonSerializable.h"
+#include "Serialization/BeamJsonUtils.h"
+
+#include "UpgradeItemRequestArgs.generated.h"
+
+UCLASS(BlueprintType, Category="Beam", DefaultToInstanced, EditInlineNew)
+class UNREALSUISAMPLEMICROSERVICECLIENTS_API UUpgradeItemRequestArgs : public UObject, public IBeamJsonSerializableUObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName="Gamer Tag", Category="Beam")
+	int64 GamerTag = {};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName="Item Instace ID", Category="Beam")
+	int64 ItemInstaceID = {};
+
+	
+
+	virtual void BeamSerializeProperties(TUnrealJsonSerializer& Serializer) const override;
+	virtual void BeamSerializeProperties(TUnrealPrettyJsonSerializer& Serializer) const override;
+	virtual void BeamDeserializeProperties(const TSharedPtr<FJsonObject>& Bag) override;
+	
+};
